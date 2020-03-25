a3b7d0a169fd86ddf69c2e0c2017820873dc5fa8
Jean Osco <willjean29@gmail.com>
2020-03-25 11:48:54 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+require('./config/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.get('/usuario', function (req, res) {
+  res.json('get usuario')
+})
+
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok: false,
+            message: 'El nombre es necesario'
+        })
+    }else{
+        res.json({
+            body
+        })
+    }
+
+})
+
+app.put('/usuario/:id', function (req, res) {
+    let {id} = req.params;
+    res.json({
+        id
+    })
+})
+
+app.delete('/usuario', function (req, res) {
+    res.json('delete usuario')
+})
+ 
+app.listen(process.env.PORT,() => {
+    console.log('Escuchando el puerto ', process.env.PORT);
+})
